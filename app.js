0f44f730262da3224b6cff8f3e934c57392d2125
zivserphos <zivfromisrael@gmail.com>
2021-11-12 10:23:03 +0200

    connected to mongo

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const env = require("dotenv").config();
+const url = process.env.URL
+
+mongoose.connect(url);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
 
 app.use(express.json());
 app.use("/", express.static(path.resolve(`./dist`)));
---
+++ mongo/students.js
@@ -0,0 +1,69 @@
+const fs = require("fs/promises")
+
+
+
+const a = [{
+
+    name: "Ido",
+    surName: "Arbel",
+    birth: "26/01/1998",
+    phone: "0526305421",
+    gender: "Male",
+    courses: [
+      "Java",
+      "Math",
+    ]} , 
+    
+    {
+    name: "Chen" ,
+    surName: "Halevi" ,
+    birth: "11/03/1997" ,
+    phone : "0526323421",
+    gender : "Male" ,
+    courses : [
+      "Math",
+      "Law"
+    ]} , 
+    
+    {
+    name : "Koren" , 
+    surName : "Gan-or",
+    birth : "19/01/1997",
+    phone : "0526305321",
+    gender : "Male",
+    courses : [
+      "JavaScript",
+      "Finance",
+      "Law"
+    ]} , 
+      
+      {
+      name: "Oryan",
+    surName: "Levy",
+    birth: "02/04/1998",
+    phone: "0542305321",
+    gender: "Male",
+    courses: [
+      "JavaScript",
+      "Law"
+    ]} ,
+    
+    {
+      name: "Yahalom",
+    surName: "Cohen",
+    birth: "03/11/1993",
+    phone: "0542305392",
+    gender: "Female",
+    courses: [
+      "Java",
+      "Law"
+    ]}   
+] 
+
+async function writeFile()
+{
+    await fs.writeFile("zzz.js" , JSON.stringify(a))
+
+}
+
+writeFile();
